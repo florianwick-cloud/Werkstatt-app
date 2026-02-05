e4d8c06743a142defda0bafb60e6837c59956f0c
Florian <florian.wick@die-therapeuten-kulmbach.de>
2026-02-05 16:16:59 +0100

    bilder speichern 17

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+// Leerer Service Worker, der ALLE alten Caches löscht
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    )
+  );
+});
