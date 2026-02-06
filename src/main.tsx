fc4f195e8f64aadeb9dc562abbaab076776112bf
Florian <florian.wick@die-therapeuten-kulmbach.de>
2026-02-06 16:10:01 +0100

    serviceworker out

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,7 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-// PWA-Registrierung über vite-plugin-pwa
-import { registerSW } from "virtual:pwa-register";
-
-registerSW({
-  onNeedRefresh() {
-    console.log("Neue Version verfügbar");
-  },
-  onOfflineReady() {
-    console.log("App ist offline bereit");
-  },
-});
+// ❗ WICHTIG: KEIN Service Worker, KEIN registerSW, KEIN unregister
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HashRouter>
